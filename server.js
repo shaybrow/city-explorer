f74cd6d1cea5588ef5d1129ee751806cdfb14172
Seamus Brown <shay_brown_13@hotmail.com>
2021-01-18 15:34:52 -0800

    server setup

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3333;
